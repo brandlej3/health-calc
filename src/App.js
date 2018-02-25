7cbd04c1e3a6974c7494712ffc6d55b6851c25df
brandlej3 <bjones350@gatech.edu>
2018-02-25 18:34:43 -0500

    modal windows for activities and CSS changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
-import { Button, Grid, Row, Col, Jumbotron } from 'react-bootstrap';
+import { Modal, Button, Grid, Row, Col, Jumbotron } from 'react-bootstrap';
 import PropTypes from 'prop-types';
 import { CalorieDisplay } from './Display.js';
 
 class App extends Component {
   state = {
+    showToggle: false,
+    theContent: "",
     clicked: false,
     gender: null,
     weight: null,
@@ -48,6 +50,36 @@ class App extends Component {
   renderCalorieDisplay() {
     return < CalorieDisplay stateArray={this.state} />;
   }
+  handleMouseOut = () => {
+    this.setState({
+      showToggle : false
+    });
+  }
+  handleHover = (e) => {
+    var modalContent = "";
+
+    switch(e.target.id){
+      case "sedentary":
+        modalContent = "Choose this if day-to-day you are typically sitting, or not active.";
+        break;
+      case "light":
+        modalContent = "Choose this if you go do light activity like the occasional walk / exercise.";
+        break;
+      case "moderate":
+        modalContent = "Moderate activity would be exercise more than one to two times per week; consisting of an elevated heart rate and/or strenth training.";
+        break;
+      case "very":
+        modalContent = "Your job is physically intensive and/or you work out almost everyday of the week.";
+        break;
+      default:
+        modalContent = "n/a";
+    }
+
+    this.setState({
+      showToggle : true,
+      theContent : modalContent
+    });
+  }
   render() {
     return (
       <form onSubmit={ this.handleCalculate }>
@@ -61,40 +93,46 @@ class App extends Component {
           </Row>
           <Row>
             <Col xs={12} md={12}>
-                  <h3 className="gender-group">Gender</h3>
-                  <div onClick={this.handleGender} className="gender-group">
-                    <div className={this.state.gender === "male" ? 'selected' : ''} id="male"></div>
-                    <div className={this.state.gender === "female" ? 'selected' : ''} id="female"></div>
-                  </div>
+              <h2 className="subheader-text">Calculator to see how much you should eat everyday.</h2>
+              <p className="sub-subheader-text">Consult a doctor before making any extreme changes</p>
             </Col>
           </Row>
           <Row>
-            <Col xs={12} md={12}>
+            <Col className="gender" xs={12} md={12}>
+              <h3 className="gender-group">Gender</h3>
+              <div onClick={this.handleGender} className="gender-group">
+                <div className={this.state.gender === "male" ? 'selected' : ''} id="male"></div>
+                <div className={this.state.gender === "female" ? 'selected' : ''} id="female"></div>
+              </div>
+            </Col>
+          </Row>
+          <Row>
+            <Col className="weightGrouping" xs={12} md={12}>
               <h3 className="input-labels">Weight</h3>
               <input name="weight" type="number"></input>
             </Col>
           </Row>
           <Row>
-            <Col xs={12} md={12}>
+            <Col className="heightGrouping" xs={12} md={12}>
               <h3 className="input-labels">Height</h3>
               <input name="height" type="number"></input>
             </Col>
           </Row>
           <Row>
-            <Col xs={12} md={12}>
+            <Col className="ageGrouping" xs={12} md={12}>
               <h3 className="input-labels">Age</h3>
               <input name="age" type="number"></input>
             </Col>
           </Row>
           <Row>
-            <Col xs={12} md={12}>
+            <Col className="activityGrouping" xs={12} md={12}>
               <h4>Activity Level</h4>
-                <div onClick= {this.handleActivityLevel}>
-                  <div className={this.state.activityLevel === "sedentary" ? 'activity-grid selected' : 'activity-grid'} id="sedentary"></div>
-                  <div className={this.state.activityLevel === "light" ? 'activity-grid selected' : 'activity-grid'} id="light"></div>
-                  <div className={this.state.activityLevel === "moderate" ? 'activity-grid selected' : 'activity-grid'} id="moderate"></div>
-                  <div className={this.state.activityLevel === "very" ? 'activity-grid selected' : 'activity-grid'} id="very"></div>
-                </div>
+              <div onClick= {this.handleActivityLevel}>
+                <div onClick={this.handleHover} className={this.state.activityLevel === "sedentary" ? 'activity-grid selected' : 'activity-grid'} id="sedentary"></div>
+                <div onClick={this.handleHover} className={this.state.activityLevel === "light" ? 'activity-grid selected' : 'activity-grid'} id="light"></div>
+                <div onClick={this.handleHover} className={this.state.activityLevel === "moderate" ? 'activity-grid selected' : 'activity-grid'} id="moderate"></div>
+                <div onClick={this.handleHover} className={this.state.activityLevel === "very" ? 'activity-grid selected' : 'activity-grid'} id="very"></div>
+              </div>
             </Col>
           </Row>
           <Row>
@@ -104,6 +142,23 @@ class App extends Component {
             {this.state.clicked ? this.renderCalorieDisplay() : ""}
           </Row>
         </Grid>
+        
+        <Modal show={this.state.showToggle} onHide={this.handleMouseOut} >
+            <Modal.Header>
+            <Modal.Title>
+                {this.state.activityLevel}
+            </Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+            <h4>Description</h4>
+            <p>
+                {this.state.theContent}
+            </p>
+            </Modal.Body>
+            <Modal.Footer>
+            <Button onClick={this.handleMouseOut}>Close</Button>
+            </Modal.Footer>
+        </Modal >
       </form>
     );
   }
---
+++ src/Model.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Modal, Button } from 'react-bootstrap';
+import './Model.css';
+
+export class ModelWindow extends React.Component {
+    state = {
+        show: this.props.toggle
+    };
+    render() {
+      return (
+
+      );
+    }
+  }
+export default ModelWindow;
