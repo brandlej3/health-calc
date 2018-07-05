0389dcab6e9500d12888accd202938578b36d734
brandlej3 <bjones350@gatech.edu>
2018-07-04 22:59:27 -0400

    remove react router

--- src/index.js
+++ src/index.js
@@ -2,10 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import { App } from './components/App/App.js';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 ReactDOM.render(
-    <Router>
-        <App />
-    </Router>, document.getElementById('root')
+        <App />, document.getElementById('root')
 );
