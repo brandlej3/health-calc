7476b7a47047cbb6914c5135c35a68372ed21890
brandlej3 <bjones350@gatech.edu>
2018-07-06 21:18:57 -0400

    adding mocked out tabs

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,10 +2,23 @@ import React from 'react';
 import './App.css';
 import Form from './../Form/Form';
 import Header from './../Header/Header';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+
+
 export const App = () =>
   (
     <div>
       < Header />
-      < Form />
+      <Tabs>
+        <TabList className="tabholder">
+          <Tab className="tabs">Calories</Tab>
+          <Tab className="tabs">Macros</Tab>
+        </TabList>
+        <TabPanel>
+            < Form />
+        </TabPanel>
+        <TabPanel>
+        </TabPanel>
+      </Tabs>
     </div>
   )
